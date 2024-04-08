d7d1abfa265fae230191798df0e1803ccafcc12c
dokento <dokento@ukr.net>
2024-04-08 12:48:27 +0200

    commit

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,19 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    with open("sequential.json", mode= "r") as data_file:
+        data  = json.load(data_file)
+    for key in data.keys():
+        if field == key:
+            sequential_data = data[field]
+            return sequential_data
+    else:
+        return None
 
 def main():
-    pass
+   sequential_data = read_data("sequential.json", "unordered_numbers")
+   print("sequential Data:", sequential_data)
+
 
 
 if __name__ == '__main__':
