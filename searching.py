9438ec81adffe47af511f0119332bee8444da7c6
dokento <dokento@ukr.net>
2024-04-08 12:54:46 +0200

    commit

--- searching.py
+++ searching.py
@@ -23,6 +23,21 @@ def read_data(file_name, field):
     else:
         return None
 
+def linear_search(sequence, target):
+    """
+Function sequenatialy iterates over sequence and stores position and total count of matches.
+    :param sequence: (list): list on numbers
+    :param target: (int): number to match within sequence
+    :return: (dict): {'positions': <list of indices>, 'count': <total count>,}
+    """
+    positions = []
+    count = 0
+    for i, num in enumerate(sequence):
+        if num == target:
+            positions.append(i)
+            count += 1
+
+    return{"positions": positions, "count": count}
 def main():
    sequential_data = read_data("sequential.json", "unordered_numbers")
    print("sequential Data:", sequential_data)
