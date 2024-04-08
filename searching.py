2a208686675eccf5e2bfd04d660f3a6c27673958
dokento <dokento@ukr.net>
2024-04-08 13:08:51 +0200

    commit

--- searching.py
+++ searching.py
@@ -38,6 +38,32 @@ Function sequenatialy iterates over sequence and stores position and total count
             count += 1
 
     return{"positions": positions, "count": count}
+
+def pattern_search(sequence, pattern):
+    """
+        Function sequenatialy iterates over sequence and stores position and total count of matches.
+        :param sequence: (str): string of basis ACTG
+        :param pattern: (str): string pattern to match within sequence
+        :return: (dict): {'positions': <list of indices>, 'count': <total count>,}
+        """
+    indices = set()
+    pattern_size = len(pattern)
+
+    left_idx = 0
+    right_idx = pattern_size
+    while right_idx < len(sequence):
+        for idx in range(pattern_size):
+            if pattern[idx] != sequence[left_idx + idx]:
+                break
+        else:
+            indices.add(left_idx + pattern_size // 2)
+
+        left_idx += 1
+        right_idx += 1
+
+    return indices
+
+
 def main():
    sequential_data = read_data("sequential.json", "unordered_numbers")
    print("sequential Data:", sequential_data)
